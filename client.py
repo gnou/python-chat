ef53b369d6168e29c909f3862a6fcc084d155382
Malvin Cui <malvin.cui@gmail.com>
2014-01-11 18:07:51 +0800

    format codes

--- client.py
+++ client.py
@@ -1,23 +1,24 @@
-
-m socket import *
+#!/usr/bin/env python3
+from socket import *
 from time import ctime
 import threading
+
 HOST='172.16.45.135'
 PORT=21569
 BUFSIZ=1024
-ADDR=(HOST,PORT)  #服务器的地址与端口
+ADDR=(HOST,PORT)
 
-tcpCliSock=socket(AF_INET,SOCK_STREAM) #生成客户端的套接字，并连上服务器
-tcpCliSock.connect(ADDR)
+tcpCliSock=socket(AF_INET,SOCK_STREAM)  #socket
+tcpCliSock.connect(ADDR)                #connect
 
 while True:
-        data=raw_input('> ')
-            if not data:
-                        break
-                        tcpCliSock.send('[%s] %s' %(ctime(),data)) #发送时间与数据
-                            data=tcpCliSock.recv(BUFSIZ) #接收服务器的发送过来的数据
-                                if not data:
-                                            break
-                                            print data
+    data=raw_input('> ')
+    if not data:
+        break
+    tcpCliSock.send('[%s] %s' %(ctime(),data))  #send
+    data=tcpCliSock.recv(BUFSIZ)                #receive
+    if not data:
+        break
+    print data
 
-                                            tcpCliSock.close()
+tcpCliSock.close()                      #close
--- server.py
+++ server.py
@@ -8,19 +8,19 @@ PORT=21567
 BUFSIZ=1024
 ADDR=(HOST,PORT)
 
-tcpSerSock=socket(AF_INET,SOCK_STREAM) #生成tcp服务器的套接字
-tcpSerSock.bind(ADDR) #把套接字绑定到服务器的地址上
-tcpSerSock.listen(5)  #最多允许5个连接同时连进来，后来的连接就会被拒绝
+tcpSerSock=socket(AF_INET,SOCK_STREAM)  #socket
+tcpSerSock.bind(ADDR)                   #bind
+tcpSerSock.listen(5)                    #listen
 
 while True:
-        print 'waiting for connection...'
-            tcpCliSock,addr=tcpSerSock.accept() #等待连接（被动等待），阻塞式的
-                print '...connected from:',addr
+    print 'waiting for connection...'
+    tcpCliSock.addr=tcpSerSock.accept() #accept
+    print '...connected from:',addr
 
-                    while True:
-                                data=tcpCliSock.recv(BUFSIZ)
-                                        if not data:
-                                                        break
-                                                            tcpCliSock.send('[%s] %s' %(ctime(),data))
+    while True:
+        data=tcpCliSock.recv(BUFSIZ)    #receive
+        if not data:
+            break
+        tcpCliSock.send('[%s] %s' %(ctime(),data))  #send
 
-                                                            tcpCliSock.close()
+tcpCliSock.close()                      #close
